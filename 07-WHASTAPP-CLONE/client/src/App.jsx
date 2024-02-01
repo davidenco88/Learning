e45fc601a6f1ce64fe5458f049a2b5249a2e80e5
davidenco88 <122131492+davidenco88@users.noreply.github.com>
2024-01-31 22:26:10 -0500

    feat: :construction: Add app component

--- 07-WHASTAPP-CLONE/client/src/App.jsx
+++ 07-WHASTAPP-CLONE/client/src/App.jsx
@@ -3,8 +3,12 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import Messenger from "./components/Messenger";
 
 function App() {
+  //Configurar archivo .env para manejar estas credenciales
+  const clientId =
+    "415340460413-d0ubl0ntctt7hvu60cn44fsrv8flk1sm.apps.googleusercontent.com";
+
   return (
-    <GoogleOAuthProvider>
+    <GoogleOAuthProvider clientId={clientId}>
       <Messenger />
     </GoogleOAuthProvider>
   );
