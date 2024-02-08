f07653acfbb22b13f9af7b4e56b678ef1b407836
davidenco88 <122131492+davidenco88@users.noreply.github.com>
2024-02-08 14:45:00 -0500

    feat: :construction_worker: Update Messenger component to use context

--- 07-WHASTAPP-CLONE/client/src/components/Messenger.jsx
+++ 07-WHASTAPP-CLONE/client/src/components/Messenger.jsx
@@ -1,26 +1,47 @@
+import { useContext } from "react";
+import { AccountContext } from "../context/AccountProvider";
 import { AppBar, Toolbar, Box, styled } from "@mui/material";
 
 import LoginDialog from "./account/LoginDialog";
+import ChatDialog from "./chat/ChatDialog";
 
 const Component = styled(Box)`
   height: 100vh;
   background-color: #f0f2f5;
 `;
 
-const Header = styled(AppBar)`
+const LoginHeader = styled(AppBar)`
   height: 222px;
   background-color: #00a884;
   box-shadow: none;
 `;
 
+const ChatHeader = styled(AppBar)`
+  height: 127px;
+  background-color: #00a884;
+  box-shadow: none;
+`;
+
 function Messenger() {
+  const { account } = useContext(AccountContext);
   return (
     <>
       <Component>
-        <Header>
-          <Toolbar></Toolbar>
-        </Header>
-        <LoginDialog />
+        {account ? (
+          <>
+            <ChatHeader>
+              <Toolbar></Toolbar>
+            </ChatHeader>
+            <ChatDialog />
+          </>
+        ) : (
+          <>
+            <LoginHeader>
+              <Toolbar></Toolbar>
+            </LoginHeader>
+            <LoginDialog />
+          </>
+        )}
       </Component>
     </>
   );
